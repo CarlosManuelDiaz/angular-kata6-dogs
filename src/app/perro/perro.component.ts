7e47cfa5799860ff20d6b32eff12b04b3e8c437c
Williams Aguilera <webnator@gmail.com>
2019-03-18 17:21:52 +0100

    FIX: Añadido el unsubscribe

--- src/app/perro/perro.component.ts
+++ src/app/perro/perro.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DogService } from '../dog.service';
-import { Observable, ObjectUnsubscribedError } from 'rxjs';
+import { Observable, ObjectUnsubscribedError, Subscription } from 'rxjs';
 import { Perro } from '../perro';
 
 @Component({
@@ -9,25 +9,36 @@ import { Perro } from '../perro';
   styleUrls: ["./perro.component.scss"]
 })
 export class PerroComponent implements OnInit {
-  public perros: Array<Perro>;
-
   public rutaImagenPerro: string;
+  private miSubsc: Subscription;
 
   constructor(private dogService: DogService) {}
 
   ngOnInit() {}
 
-  nuevoperro() {
-    setInterval(() => {
-      this.dogService.nuevoPerro().subscribe({
-        next: val => {
-          this.rutaImagenPerro = val.message;
-        }
-      });
-    }, 5000);
-    return {
-      unsubscribe() {}
-    };
+  public comienzaPerro(): void {
+    this.miSubsc = this.nuevoperro().subscribe();
+  }
+
+  public paraPerro(): void {
+    this.miSubsc.unsubscribe();
+  }
+
+  private nuevoperro(): Observable<void> {
+    return new Observable(() => {
+      const interval = setInterval(() => {
+
+        this.dogService.nuevoPerro().subscribe({
+          next: val => {
+            this.rutaImagenPerro = val.message;
+          }
+        });
+
+      }, 5000);
+      return {
+        unsubscribe() { clearInterval(interval) }
+      };
+    });
   }
 }
 
